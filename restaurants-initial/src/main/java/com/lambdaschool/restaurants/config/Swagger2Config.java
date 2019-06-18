47dfc2c353a1ebb499728d0fd3d7a2139c5f3c52
Alfonso <alfonso.garcia2324@gmail.com>
2019-06-18 10:44:34 -0700

    Added .ignoredParameterTypes after .build in Swagger2Config

--- restaurants-initial/src/main/java/com/lambdaschool/restaurants/config/Swagger2Config.java
+++ restaurants-initial/src/main/java/com/lambdaschool/restaurants/config/Swagger2Config.java
@@ -2,6 +2,7 @@ package com.lambdaschool.restaurants.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,7 +24,10 @@ public class Swagger2Config
                 .apis(RequestHandlerSelectors
                         .basePackage("com.lambdaschool.restaurants"))
                 .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo());
+                .build()
+                .ignoredParameterTypes(Pageable.class)
+                .apiInfo(apiEndPointsInfo());
+
     }
 
     private ApiInfo apiEndPointsInfo()
